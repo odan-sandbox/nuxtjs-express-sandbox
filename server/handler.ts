01e3d2d58c6b6b0ca554bda1c718c099345ca146
odan <odan3240@gmail.com>
2020-07-26 19:34:36 +0900

    add serverless

--- server/handler.ts
+++ server/handler.ts
@@ -3,8 +3,29 @@ import { Handler } from 'aws-lambda'
 
 import { app } from './app'
 
-const server = awsServerlessExpress.createServer(app)
+const binaryMimeTypes = [
+  'application/javascript',
+  'application/json',
+  'application/octet-stream',
+  'application/xml',
+  'font/eot',
+  'font/opentype',
+  'font/otf',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'text/comma-separated-values',
+  'text/css',
+  'text/html',
+  'text/javascript',
+  'text/plain',
+  'text/text',
+  'text/xml'
+]
+
+const server = awsServerlessExpress.createServer(app, undefined, binaryMimeTypes)
 
 export const handler: Handler = (event, context) => {
+  console.log('event', JSON.stringify(event))
   awsServerlessExpress.proxy(server, event, context)
 }
